0b4ed2af8bd5afa11fdad61350d8a3c41b4e915b
Lance W <lwakeling@gmail.com>
2017-04-16 17:24:03 -0400

    :wastebasket:

--- test/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Notebooks.PageControllerTest do
-  use Notebooks.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-  end
-end
--- web/controllers/page_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule Notebooks.PageController do
-  use Notebooks.Web, :controller
-
-  def index(conn, _params) do
-    render conn, "index.html"
-  end
-end
